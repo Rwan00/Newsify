b5a12eb82e71ae2af3d301b199043ef40be060be
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-02-25 00:01:07 +0200

    test

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/latest_news_item.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      appBar: AppBar(
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(12.0),
+            child: CircleAvatar(
+              backgroundColor: Color.fromRGBO(255, 94, 102, 1),
+              radius: 26,
+              child: Icon(Icons.search_rounded,color: Colors.white,),
+            ),
+          ),
+        ],
+      ),
+      body: const LatestNewsItem(),
+    );
   }
 }
---
+++ lib/widgets/latest_news_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LatestNewsItem extends StatelessWidget {
+  const LatestNewsItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 100,
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage("assets/kk.jpg"),
+        )
+      ),
+    );
+  }
+}
