d1026cbd35c5c29dc2ab10cb2db9de1a0360bde7
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-03-19 06:15:08 +0200

    add web view screen

--- lib/widgets/latest_news_item.dart
+++ lib/widgets/latest_news_item.dart
@@ -3,42 +3,49 @@ import 'package:newsify/models/article_model.dart';
 
 import 'package:newsify/theme/theme.dart';
 
+import '../screens/web_view_screen.dart';
+
 class LatestNewsItem extends StatelessWidget {
   final ArticleModel article;
   const LatestNewsItem({super.key, required this.article});
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 16.0, left: 8),
-      child: Container(
-        height: 240,
-        width: 345,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(
-              article.img ??
-                  "https://i.pinimg.com/564x/fc/7e/ce/fc7ece8e8ee1f5db97577a4622f33975.jpg",
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context)=>WebViewScreen(url: article.articleUrl!,),),);
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(right: 16.0, left: 8),
+        child: Container(
+          height: 240,
+          width: 345,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: NetworkImage(
+                article.img ??
+                    "https://i.pinimg.com/564x/fc/7e/ce/fc7ece8e8ee1f5db97577a4622f33975.jpg",
+              ),
+              fit: BoxFit.cover,
+              opacity: 0.6,
             ),
-            fit: BoxFit.cover,
-            opacity: 0.6,
+            borderRadius: BorderRadius.circular(30),
           ),
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                article.title!,
-                style: titleStyle,
-              ),
-              Text(
-                article.author != null ? "By ${article.author}" : "",
-                style: subTitle.copyWith(color: Colors.white),
-              ),
-            ],
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  article.title!,
+                  style: titleStyle,
+                ),
+                Text(
+                  article.author != null ? "By ${article.author}" : "",
+                  style: subTitle.copyWith(color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/widgets/news_tile.dart
+++ lib/widgets/news_tile.dart
@@ -2,44 +2,51 @@ import 'package:flutter/material.dart';
 import 'package:newsify/models/article_model.dart';
 import 'package:newsify/theme/theme.dart';
 
+import '../screens/web_view_screen.dart';
+
 class NewsTile extends StatelessWidget {
   final ArticleModel article;
   const NewsTile({required this.article,super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(15),
-          child: Image.network(article.img ??
-                  "https://i.pinimg.com/564x/fc/7e/ce/fc7ece8e8ee1f5db97577a4622f33975.jpg"),
-        ),
-        Text(
-          article.title??"",
-          style: titleStyle,
-          maxLines: 3,
-          overflow: TextOverflow.ellipsis,
-        ),
-        Row(
-          children: [
-            Text(
-              article.author??"",
-              style: subTitle,
-            ),
-            const Spacer(),
-            Text(
-              article.date!.substring(0,10),
-              style: subTitle,
-            ),
-          ],
-        ),
-         Container(
-              height: 3,
-              width: double.infinity,
-              color: Colors.grey,
-            )
-      ],
+    return GestureDetector(
+       onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context)=>WebViewScreen(url: article.articleUrl!,),),);
+      },
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(15),
+            child: Image.network(article.img ??
+                    "https://i.pinimg.com/564x/fc/7e/ce/fc7ece8e8ee1f5db97577a4622f33975.jpg"),
+          ),
+          Text(
+            article.title??"",
+            style: titleStyle,
+            maxLines: 3,
+            overflow: TextOverflow.ellipsis,
+          ),
+          Row(
+            children: [
+              Text(
+                article.author??"",
+                style: subTitle,
+              ),
+              const Spacer(),
+              Text(
+                article.date!.substring(0,10),
+                style: subTitle,
+              ),
+            ],
+          ),
+           Container(
+                height: 3,
+                width: double.infinity,
+                color: Colors.grey,
+              )
+        ],
+      ),
     );
   }
 }
